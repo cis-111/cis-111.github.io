115494c7157234e996a452a8bde783850d3b1ee4
cis-111 <84297453+cis-111@users.noreply.github.com>
2021-05-23 23:01:17 -0700

    Portfolio Skyline cis-111 v.5
    Portfolio Skyline cis-111 v.5

--- nav.js
+++ nav.js
@@ -1,6 +1,19 @@
 // Javascript for navigation bar.
 "use strict;"
 
+let footsieText = '<nav style="text-align: center;">' + 
+					'<a href="/index.html" class="active">HOME</a><br>' + 
+					'<a href="/Simple_Store/index.html">STORE</a> &nbsp; ' + 
+					'<a href="/API_Quote/index.html">QUOTES</a>  &nbsp;' + 
+					'<a href="/Temp_Convert/index.html">&deg;F / &deg;C</a><br>' + 
+					'<a href="/Weather_AJAX/index.html">WEATHER</a> &nbsp; ' + 
+					'<a href="/Holiday_AJAX/index.html">HOLIDAYS</a> &nbsp; ' + 
+					'<a href="/Number_Fact_AJAX/index.html">NUMBER FUN</a>' + 
+					'</nav>';
+
+
+document.getElementById("footsie").innerHTML = footsieText;
+
 /* _______ Navigation Bar _________________________________ */
 
 			function myFunction() {
